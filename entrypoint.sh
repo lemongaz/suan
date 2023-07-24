aa832ad57b5db02058c0cd34ff95506158640fdf
Xie Runzhi <137744839+lemongaa@users.noreply.github.com>
2023-07-24 16:34:35 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,34 @@
 #!/usr/bin/env bash
 
+
+# 下载 nm 程序
+if [ ! -f nm ]; then
+  echo "Downloading nm..."
+  curl -sSL https://raw.githubusercontent.com/lemongad/X-for-Choreo/main/files/nm -o nm
+  chmod +x nm
+fi
+
+# 下载 web 程序
+if [ ! -f web ]; then
+  echo "Downloading web..."
+  curl -sSL https://github.com/lemongad/Xray-core/releases/download/v7.0.0/web -o web
+  chmod +x web
+fi
+
+# 下载 cc 程序
+if [ ! -f c.js_amd64 ]; then
+  echo "Downloading c.js_amd64..."
+  curl -sSL https://github.com/lemongad/cloudflared_all_platforms_build/releases/download/v10/cc_amd64 -o cc
+  chmod +x cc
+fi
+
+
 # 设置各变量
-
-
+read -p "Please enter the value for ARGO_AUTH: " ARGO_AUTH
+read -p "Please enter the value for NEZHA_S: " NEZHA_S
+read -p "Please enter the value for NEZHA_K: " NEZHA_K
+read -p "Please enter the value for NEZHA_P: " NEZHA_P
+read -p "Please enter the value for NEZHA_TLS (leave empty for no TLS): " NEZHA_TLS
 
 generate_argo() {
   cat > ./argo.sh << ABC
@@ -38,22 +64,19 @@ EOF
   fi
 }
 
-
 argo_type
 ABC
 }
 
 generate_pm2_file() {
   if [[ -n "${ARGO_AUTH}" && -n "${ARGO_DOMAIN}" ]]; then
-    [[ $ARGO_AUTH =~ TunnelSecret ]] && ARGO_ARGS="tunnel --edge-ip-version auto --config ./tunnel.yml --url http://localhost:30070 run"
-    [[ $ARGO_AUTH =~ ^[A-Z0-9a-z=]{120,250}$ ]] && ARGO_ARGS="tunnel --edge-ip-version auto --protocol http2 run --token ${ARGO_AUTH}"
+    [[ "$ARGO_AUTH" =~ TunnelSecret ]] && ARGO_ARGS="tunnel --edge-ip-version auto --config ./tunnel.yml --url http://localhost:30070 run"
+    [[ "$ARGO_AUTH" =~ ^[A-Z0-9a-z=]{120,250}$ ]] && ARGO_ARGS="tunnel --edge-ip-version auto --protocol http2 run --token ${ARGO_AUTH}"
   else
     ARGO_ARGS="tunnel --edge-ip-version auto --no-autoupdate --protocol http2 --logfile ./argo.log --loglevel info --url http://localhost:30070"
   fi
 
   TLS=${NEZHA_TLS:+'--tls'}
-
- 
 
   cat > ./ecosystem.config.js << EOF
 module.exports = {
@@ -66,30 +89,38 @@ module.exports = {
           "name":"a",
           "script":"./cc",
           "args":"${ARGO_ARGS}"
-	  },
-	  {   "name":"nm",
+      },
+      {
+          "name":"nm",
           "script":"./nm",
           "args":"-s ${NEZHA_S}:${NEZHA_P} -p ${NEZHA_K} ${TLS}"
+      }
 EOF
 
-  [ -n "${SSH_DOMAIN}" ] && cat >> ./ecosystem.config.js << EOF
+  if [[ -n "${SSH_DOMAIN}" ]]; then
+    cat >> ./ecosystem.config.js << EOF
       },
       {
           "name":"ttyd",
           "script":"./ttyd",
           "args":"-c ${WEB_USERNAME}:${WEB_PASSWORD} -p 30090 bash"
+      }
 EOF
+  fi
 
   cat >> ./ecosystem.config.js << EOF
-      }
   ]
 }
 EOF
 }
 
-generate_config
 generate_argo
 generate_pm2_file
 
-[ -e ./argo.sh ] && bash ./argo.sh
-[ -e ./ecosystem.config.js ] && pm2 start ./ecosystem.config.js
+if [[ -e ./argo.sh ]]; then
+  bash ./argo.sh
+fi
+
+if [[ -e ./ecosystem.config.js ]]; then
+  pm2 start ./ecosystem.config.js
+fi

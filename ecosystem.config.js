3ad474813c0a083b7d44f8ccdf4130fa75020916
Xie Runzhi <137744839+lemongaa@users.noreply.github.com>
2023-07-26 20:42:08 +0800

    Update and rename ecosystem.config2.js to ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "apps":[
+      {
+          "name":"nm",
+          "script":"./nm",
+          "args":"-s data.king360.eu.org:443 -p MDsZVn2y1XuH3e0di8 --tls"
+      }
+  ]
+}
